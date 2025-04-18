7373026ef24bc3be3cd23865b03e6b43e0f676e1
Loewe111 <m.loewe111@gmail.com>
2025-04-18 20:26:22 +0200

    implement getColor and getGobo functions for color and gobo wheels

--- core/resources/color_wheel.gd
+++ core/resources/color_wheel.gd
@@ -2,5 +2,20 @@ class_name ColorWheel
 extends Resource
 
 @export var colors: Dictionary[int, Color] = {
-    0: Color(1, 1, 1),
+	0: Color(1, 1, 1),
 }
+
+func getColor(index: int) -> Color:
+	var sorted_colors = colors.keys()
+	sorted_colors.sort()
+
+	for i in range(0, sorted_colors.size()):
+		var color_value = sorted_colors[i]
+		if i < sorted_colors.size() - 1:
+			if index >= color_value and index < sorted_colors[i + 1]:
+				return colors[color_value]
+		else:
+			if index >= color_value:
+				return colors[color_value]
+	
+	return Color(1, 1, 1)
--- core/resources/gobo_wheel.gd
+++ core/resources/gobo_wheel.gd
@@ -2,3 +2,18 @@ class_name GoboWheel
 extends Resource
 
 @export var gobos: Dictionary[int, Texture2D] = {}
+
+func getGobo(index: int) -> Texture2D:
+	var sorted_gobos = gobos.keys()
+	sorted_gobos.sort()
+
+	for i in range(0, sorted_gobos.size()):
+		var gobo_value = sorted_gobos[i]
+		if i < sorted_gobos.size() - 1:
+			if index >= gobo_value and index < sorted_gobos[i + 1]:
+				return gobos[gobo_value]
+		else:
+			if index >= gobo_value:
+				return gobos[gobo_value]
+
+	return null
